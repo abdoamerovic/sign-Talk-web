57af06e83ba90660d147d1a42c1f093eaefded38
Abdo Amer <abdelrahmanamer400@gmail.com>
2026-07-11 02:49:54 -0900

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,5 @@ export default defineConfig({
     include: ['@mediapipe/hands', '@mediapipe/camera_utils'],
   },
 
-  // 🔥 ده أهم سطر (انسخه زي ما هو)
-  base: '/signtalk/sign-talk-web/',
+  base: '/',
 })
